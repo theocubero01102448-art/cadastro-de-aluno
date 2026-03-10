4d71540e7ad406767fdc734cc84efea287f05795
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-10 09:35:47 -0300

    Add initial CSS styles for body, input, and button

---
+++ style.css
@@ -0,0 +1,18 @@
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    margin-top: 50px;
+    background-color: coral;
+}
+
+input {
+    display: block;
+    margin: 10px auto;
+    padding: 8px;
+}
+
+button {
+    padding: 8px 20px;
+    background-color: chartreuse;
+    border: none;
+}
