98e205d7cd9b168c77ac9afcbe6f34c3eb36a413
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-10 09:35:22 -0300

    Add salvar function to log name and age

---
+++ script.js
@@ -0,0 +1,6 @@
+function salvar() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    console.log("Nome:", nome);
+    console.log("Idade:", idade);
+};
